c418ef42904e02d57bf8676afbf0c6eac8f71963
tanurims <140165281+tanurims@users.noreply.github.com>
2025-08-02 00:26:05 +0530

    Add endpoint to fetch questions by category
    Introduced a new REST endpoint in QuestionController to retrieve questions filtered by category. Updated Question entity to use 'category' instead of 'Category'. Added corresponding method in QuestionDao and QuestionService to support this functionality.

--- quizapp/src/main/java/com/quiz/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/quiz/quizapp/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.quiz.quizapp.Question;
 import com.quiz.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -19,4 +20,13 @@ public class QuestionController {
     public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+
+    }
 }
+
+
+
--- quizapp/src/main/java/com/quiz/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/quiz/quizapp/service/QuestionService.java
@@ -17,4 +17,8 @@ public class QuestionService {
         return questionDao.findAll();
 
     }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
 }
